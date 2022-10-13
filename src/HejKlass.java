47e25b6c07d92469c6af33f74d002bcfb055478f
Sara Elframawy <sara.elfaramawy.93@gmail.com>
2022-10-13 20:01:47 +0200

    now iadded the helloworld

---
+++ src/HejKlass.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.*;
+public class HejKlass {
+    public static void main(String[] args){
+        System.out.println("hej klassen");
+        String age = JOptionPane.showInputDialog("hUR GAMMAL ÄR ?");
+        JOptionPane.showMessageDialog(null,"age is "+age);
+        JOptionPane.showMessageDialog(null,"Hello world");
+        JOptionPane.showConfirmDialog(null,"Är du säker");
+        var scan = new Scanner (System.in);
+        System.out.println("Hur gammal är du?");
+        int year = scan.nextInt();
+        int g = Integer.parseInt(age)+year;
+        System.out.println(g);
+
+
+
+    }
+}
