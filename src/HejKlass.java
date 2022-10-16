28baf00f72f1353985f1beb700311b02551f4bd0
Sara Elframawy <sara.elfaramawy.93@gmail.com>
2022-10-16 13:55:57 +0200

    updating the readme file

--- src/HejKlass.java
+++ src/HejKlass.java
@@ -6,7 +6,7 @@ public class HejKlass {
         String age = JOptionPane.showInputDialog("hUR GAMMAL ÄR ?");
         JOptionPane.showMessageDialog(null,"age is "+age);
         JOptionPane.showMessageDialog(null,"Hello world");
-        JOptionPane.showConfirmDialog(null,"Är du säker");
+        JOptionPane.showConfirmDialog(null, "Är du säker");
         var scan = new Scanner (System.in);
         System.out.println("Hur gammal är du?");
         int year = scan.nextInt();
